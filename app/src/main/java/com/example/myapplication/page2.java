cd5df5e4e376dea21cd90536b4b847afedc65783
unknown <sawsj25@gmail.com>
2022-06-14 13:48:14 +0430

    inital commit

---
+++ app/src/main/java/com/example/myapplication/page2.java
@@ -0,0 +1,17 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class page2 extends AppCompatActivity {
+    public Button btnretun;
+    public EditText ;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_page2);
+    }
+}
